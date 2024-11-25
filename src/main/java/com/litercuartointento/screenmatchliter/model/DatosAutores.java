c6c393df63c39af58cbf58cbde1456fa252c6b19
Noemi-67 <noefarre@gmail.com>
2024-11-25 13:53:49 -0300

    Add files via upload

---
+++ src/main/java/com/litercuartointento/screenmatchliter/model/DatosAutores.java
@@ -0,0 +1,14 @@
+package com.litercuartointento.screenmatchliter.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutores(
+      @JsonAlias("name") String nombre,
+      @JsonAlias("birth_year") int fechaDeNacimiento,
+      @JsonAlias ("death_year") int fechaDeFallecimiento
+) {
+}
+
+
